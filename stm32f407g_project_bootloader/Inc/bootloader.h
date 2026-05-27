e9d440b00f93a5a415286d0f57b909f812dc0a6a
cht0577 <harshitha.viswanath@tuhh.de>
2026-05-27 14:49:34 +0200

    Flash Lock & Unlock

--- stm32f407g_project_bootloader/Inc/bootloader.h
+++ stm32f407g_project_bootloader/Inc/bootloader.h
@@ -10,14 +10,43 @@
 
 #include <stdint.h>
 
+// flash control reg peripheral : control
+// is very much different from flash : storage
+#define AHB1_BASE_ADDR				0x40020000U
+#define FLASH_REG_BASE_ADDR			(0x40020000U + 0x3C00U)
+
+// bootloader
 #define BOOT_FLASH_ADDR				0x8000000U
 #define APP_FLASH_ADDR				0x08020000U
 
+// SRAM
 #define SRAM_START_ADDR				0x20000000U
 #define SRAM_END_ADDR				0x20020000U
 
 #define APP_SP_ADDR					APP_FLASH_ADDR
 
+// struct for flash inteface registers
+typedef struct
+{
+	volatile uint32_t FLASH_ACR;
+	volatile uint32_t FLASH_KEYR;
+	volatile uint32_t FLASH_OPTKEYR;
+	volatile uint32_t FLASH_SR;
+	volatile uint32_t FLASH_CR;
+	volatile uint32_t FLASH_OPTCR;
+
+}Flash_RegDef_t;
+
+#define FLASH_REG					((Flash_RegDef_t*) FLASH_REG_BASE_ADDR)
+
+// flags
+#define FLASH_CR_LOCK_FLAG    		(1U << 31)
+#define FLASH_BSY_FLAG				(1U << 16)
+// flash related APIs
+uint8_t flash_Unlock(void);
+uint8_t flash_Lock(void);
+
+// bootloader related APIs
 void bootloader_JumpToApplication(void);
 
 #endif /* BOOTLOADER_H_ */
--- stm32f407g_project_bootloader/Src/bootloader.c
+++ stm32f407g_project_bootloader/Src/bootloader.c
@@ -8,6 +8,36 @@
 #include "bootloader.h"
 
 #include <stdint.h>
+
+uint8_t flash_Unlock(void)
+{
+	/*
+	 * 1. Unlock FLASH_KEYR: proper sequence
+	 * 2. Check for lock bit in CR to be 0
+	 * lock bit is auto cleared by HW after unlock */
+	FLASH_REG->FLASH_KEYR =  0x45670123;
+	FLASH_REG->FLASH_KEYR =   0xCDEF89AB;
+	// above is a whole 32bit sequence, no need to shift by 16 for second
+
+	uint32_t timeOut = 100000U;
+	while((FLASH_REG->FLASH_CR & (FLASH_CR_LOCK_FLAG)) && timeOut--);
+
+	return (!(FLASH_REG->FLASH_CR & (FLASH_CR_LOCK_FLAG))); // no need to cache here
+}
+
+uint8_t flash_Lock(void)
+{
+	/* 1. Continuously poll the BSY bit for 0
+	 * 2. Write lock bit = 1
+	 * */
+
+	while(FLASH_REG->FLASH_SR & FLASH_BSY_FLAG);
+
+	FLASH_REG->FLASH_CR |= (1U << 31);
+
+	return 1;
+}
+
 void bootloader_JumpToApplication(void)
 {
 	uint32_t appStartAddr = APP_FLASH_ADDR;
@@ -30,3 +60,7 @@ void bootloader_JumpToApplication(void)
 	appEntry();
 
 }
+
+
+
+
